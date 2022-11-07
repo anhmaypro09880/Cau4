6563ea0027f48b30525b53b5279d34c5ff2d4ed5
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-07 13:47:12 +0700

    Initial commit

---
+++ src/main/java/com/example/cau4/Cau4Application.java
@@ -0,0 +1,13 @@
+package com.example.cau4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cau4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cau4Application.class, args);
+    }
+
+}
