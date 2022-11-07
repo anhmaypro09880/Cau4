a19350f96c729b34049c6d663ac3823f7239802e
Phan Nguyen Tan Trong <anhmaypro09880@gmail.com>
2022-11-07 13:47:46 +0700

    finish discovery service

--- src/main/java/com/example/cau4/Cau4Application.java
+++ src/main/java/com/example/cau4/Cau4Application.java
@@ -2,8 +2,10 @@ package com.example.cau4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Cau4Application {
 
     public static void main(String[] args) {
